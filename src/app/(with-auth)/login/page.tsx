a4c4cd590e9cbde326843cce3a4b9887ffcc2e08
bmspd <my_spid@mail.ru>
2025-01-06 22:45:34 +0100

    feat(ui): add  components

--- src/app/(with-auth)/login/page.tsx
+++ src/app/(with-auth)/login/page.tsx
@@ -1,3 +1,14 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+
 export default async function Login() {
-  return <div>I AM LOGIN PAGE</div>
+  return (
+    <div className="min-h-screen min-w-screen size-full relative">
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col gap-2">
+        <Input placeholder="username" />
+        <Input placeholder="password" />
+        <Button>Sign in</Button>
+      </div>
+    </div>
+  )
 }
